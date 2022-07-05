71e107613f0381a9130a32bf511589b944d7f825
Cristian Salinas <62073149+tinosalinas777@users.noreply.github.com>
2022-07-05 15:27:46 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ winecfg
 wine msiexec /i python-2.7.12.msi /L*v log.txt
 wine pywin32-220.win32-py2.7.exe
 wine pyHook-1.5.1.win32-py2.7.exe
-wine /root/.wine/drive_c/Python27/python.exe -m pip install pyinstaller
+wine /root/.wine/drive_c/Python27/python.exe -m pip install pyinstaller==3.0
